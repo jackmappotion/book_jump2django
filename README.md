458d5cf57170157e5eb6f37179d1550078efaf29
jackmappotion <jackmappotion@gmail.com>
2023-05-13 13:09:59 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Jump2Django
 
 ## 2023-05-13 ~
+
+
+## models related
+```
+from models import Question, Answer
+
+Question_sets = Question.object.all()
+q = Question_sets[0]
+print(q.subject)
+q.subject = 'new subject'
+q.save()
+```
--- jump2django/pybo/views.py
+++ jump2django/pybo/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello Django")
+    question_list = Question.objects.order_by("-create_date")
+    context = {'question_list':question_list}
+    return render(request=request, template_name='pybo/question_list.html', context=context)
+
+def detail(request, question_id):
+    question = get_object_or_404(klass=Question, pk=question_id)
+    # question = Question.objects.get(id=question_id)
+    context = {'question':question}
+    return render(request=request, template_name='pybo/question_detail.html', context=context)
